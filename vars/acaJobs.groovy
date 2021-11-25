c08756ca14344ed810bb718e86ece65c3e1e8068
Gastón Panizza <gaston.panizza@gmail.com>
2021-11-25 17:42:12 -0300

    Add executeCanaryAnalysis

--- vars/acaJobs.groovy
+++ vars/acaJobs.groovy
@@ -1,4 +1,5 @@
 #!/usr/bin/groovy
+import groovy.json.JsonSlurper
 
 def printMessage(message){
   echo "${message}"
@@ -13,7 +14,30 @@ def executeLoadTesting(){
 }
 
 def executeCanaryAnalysis(){
-  echo "ok"
+  try {
+    def body = '{"id": 120}'
+    def http = new URL("http://localhost:8090/standalone_canary_analysis/?metricsAccountName=canary-prometheus&storageAccountName=in-memory-store-account&application=Canary+Test").openConnection() as HttpURLConnection
+    http.setRequestMethod('POST')
+    http.setDoOutput(true)
+    http.setRequestProperty("Accept", '*/*')
+    http.setRequestProperty("Content-Type", 'application/json')
+
+    http.outputStream.write(body.getBytes("UTF-8"))
+    http.connect()
+
+    def response = [:]
+
+    if (http.responseCode == 200) {
+        response = new JsonSlurper().parseText(http.inputStream.getText('UTF-8'))
+    } else {
+        response = new JsonSlurper().parseText(http.errorStream.getText('UTF-8'))
+    }
+
+    println "response: ${response}"
+
+  } catch (Exception e) {
+    echo e
+  }
 }
 
 def retrieveAnalysisResults(){
