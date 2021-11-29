bde1c48387239fde88b59cb2f99cba0559098385
Gastón Panizza <gaston.panizza@gmail.com>
2021-11-29 13:52:57 -0300

    Fix precisions

--- vars/acaJobs.groovy
+++ vars/acaJobs.groovy
@@ -20,8 +20,8 @@ def printMessage(message){
   echo "${message}"
 }
 
-def applyCanaryPolicy(String groupId, String assetId, String assetVersion, String host, Integer port, String protocol, String path, String weight,
-                      String hostCanary, Integer portCanary, String protocolCanary, String pathCanary, String weightCanary){
+def applyCanaryPolicy(String groupId, String assetId, String assetVersion, String host, Integer port, String protocol, String path, Integer weight,
+                      String hostCanary, Integer portCanary, String protocolCanary, String pathCanary, Integer weightCanary){
 
   //Step 1 - Create a Proxy app (optional)
   echo "applyCanaryPolicy Step 1"
