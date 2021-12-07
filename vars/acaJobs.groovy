cb712c3dda39eefbe2c901674f1281f767bd4827
Gastón Panizza <gaston.panizza@gmail.com>
2021-12-07 14:52:38 -0300

    Test

--- vars/acaJobs.groovy
+++ vars/acaJobs.groovy
@@ -285,8 +285,9 @@ def updateCanaryTraffic(String organizationId, String environmentId, String prox
   }
   """
   println "updateCanaryTraffic()-1"
-    def authToken=commons.getAuthToken()
+  //def authToken=commons.getAuthToken()
   println "updateCanaryTraffic()-2"
-  def response = commons.executePatchWithBody("${policiesUrl}", "${authToken}", "${body}", "204", "updateCanaryTraffic")
+  //def response = commons.executePatchWithBody("${policiesUrl}", "${authToken}", "${body}", "204", "updateCanaryTraffic")
+  def response = commons.executePatchWithBody("${policiesUrl}", commons.getAuthToken(), "${body}", "204", "updateCanaryTraffic")
   return "${response}"
 }
